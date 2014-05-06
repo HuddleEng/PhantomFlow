8ad374520c5865e7ea0585e4388c07d14dbee774
James Cryer <james.cryer@huddle.com>
2014-05-06 17:17:47 +0100

    cleanup

--- lib/casperjs.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-set CASPER_PATH=%~dp0..\node_modules\phantomcss\CasperJs
-set CASPER_BIN=%CASPER_PATH%\bin\
-set PHANTOMJS=%~dp0..\node_modules\phantomcss\PhantomJs\phantomjs.exe
-set ARGV=%*
-
-call "%PHANTOMJS%" "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%

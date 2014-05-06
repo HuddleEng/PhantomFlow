39348a530fb7042ad7d6a5eb7b0acd5498677772
James Cryer <james.cryer@huddle.com>
2014-05-06 16:36:29 +0100

    fixes for report generation

--- test/test.js
+++ test/test.js
@@ -2,18 +2,26 @@
 var path = require('path');
 var connect = require('connect');
 
-connect(
-	connect.static(__dirname)
-).listen(9001);
-
 var flow = require('../lib/phantomflow').init({
-	earlyexit: true
+	earlyexit: true,
+	createReport: true
 });
 
-// flow.event.on('exit', function(){
-// 	process.exit(0);
-// });
+if(process.argv[2] && process.argv[2] == 'report'){
 
-flow.run(function(){
-	process.exit(0);
-});
+	flow.report();
+	
+} else {
+
+	connect(
+		connect.static(__dirname) // Serve the system under test for this example
+	).listen(9001);
+
+	// flow.event.on('exit', function(){
+	// 	process.exit(0);
+	// });
+
+	flow.run(function(){
+		process.exit(0);
+	});	
+}

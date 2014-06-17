d3b433bd7398dc9e45145f877834eb59362e065a
James Cryer <james.cryer@huddle.com>
2014-06-17 15:04:15 +0100

    Fixed responsive test example

--- test/flows/responsive.test.js
+++ test/flows/responsive.test.js
@@ -3,31 +3,32 @@
 
 	flow("My first responsive webpage", function(){
 		decision({
+			"View in 1500px": function(){
+				step('Look at the page', lookAtIt1500);
+			},
 			"View in 990px": function(){
 				step('Look at the page', lookAtIt990);
 			},
-			"View in 480px": function(){
-				step('Look at the page', lookAtIt480);
+			"View in 440px": function(){
+				step('Look at the page', lookAtIt440);
 			}
 		});
 	});
+
+	function lookAtIt1500 (){
+		lookAtIt(1500);
+	}
 
 	function lookAtIt990 (){
 		lookAtIt(990);
 	}
 
-	function lookAtIt480 (){
-		lookAtIt(480);
+	function lookAtIt440 (){
+		lookAtIt(440);
 	}
 
 	function lookAtIt(w){
-
-		casper.page.viewportSize = {
-			width: w,
-			height: 768
-		};
-
-		casper.thenOpen("http://localhost:9001/responsive", function(){
+		casper.viewport(w, 768).thenOpen("http://localhost:9001/responsive", function(){
 			casper.test.pass('Responsive page has loaded');
 			phantomCSS.screenshot('body');
 		});
